71590f3b92a1e9f8a4979ec8fbfeb8a54e6428d4
Art Den <wolf6806@yandex.ru>
2025-07-29 23:32:01 +0300

    Added iOS 17, 18 and macOS 13, 15 methods

---
+++ Sources/SwiftUI-Adapter/Extensions/View+Ext.swift
@@ -0,0 +1,9 @@
+//  Created by Мобильный трудоголик: https://t.me/hardworkerIT
+
+import SwiftUI
+
+public extension View {
+  var adapter: SwiftUIAdapter<Self> {
+    SwiftUIAdapter(self)
+  }
+}
--- Sources/SwiftUI-Adapter/SwiftUIAdapter.swift
+++ Sources/SwiftUI-Adapter/SwiftUIAdapter.swift
@@ -1,7 +1,185 @@
 //  Created by Мобильный трудоголик: https://t.me/hardworkerIT
 
-public struct SwiftUIAdapter {
-  public static func hello() -> String {
-    "Hello from SwiftUIAdapter!"
+import ImagePlayground
+import SwiftUI
+
+public struct SwiftUIAdapter<ContentView: View> {
+  public let content: ContentView
+  
+  public init(
+    _ content: ContentView
+  ) {
+    self.content = content
+  }
+}
+
+// MARK: iOS 17 / macOS 13
+
+@MainActor extension SwiftUIAdapter {
+  public enum SwiftUIAdapterContentTransition {
+    case identity
+    case numericText
+    case opacity
+  }
+  
+  
+  @ViewBuilder public func contentTransition(
+    _ transition: SwiftUIAdapterContentTransition
+  ) -> some View {
+#if os(iOS)
+    if #available(iOS 17.0, *) {
+      switch transition {
+      case .identity:
+        content.contentTransition(.identity)
+      case .opacity:
+        content.contentTransition(.opacity)
+      case .numericText:
+        content.contentTransition(.numericText())
+      }
+    } else {
+      content
+    }
+#elseif os(macOS)
+    if #available(macOS 13.0, *) {
+      switch transition {
+      case .identity:
+        content.contentTransition(.identity)
+      case .numericText:
+        content.contentTransition(.numericText())
+      case .opacity:
+        content.contentTransition(.opacity)
+      }
+    } else {
+      content
+    }
+#else
+    content
+#endif
+  }
+}
+
+// MARK: iOS 18 / macOS 15
+
+@MainActor extension SwiftUIAdapter {
+  public enum SwiftUIAdapterPresentationSizing {
+    case form
+  }
+  
+  @ViewBuilder public func presentationSizing(
+    _ sizing: SwiftUIAdapterPresentationSizing
+  ) -> some View {
+#if os(iOS)
+    if #available(iOS 18, *) {
+      switch sizing {
+      case .form:
+        content.presentationSizing(.form)
+      }
+    } else {
+      content
+    }
+#elseif os(macOS)
+    if #available(macOS 15.0, *) {
+      switch sizing {
+      case .form:
+        content.presentationSizing(.form)
+      }
+    } else {
+      content
+    }
+#else
+    content
+#endif
+  }
+  
+  @ViewBuilder public func navigationTransitionZoom(
+    sourceID: some Hashable,
+    in namespace: Namespace.ID
+  ) -> some View {
+#if os(iOS)
+    if #available(iOS 18, *) {
+      content
+        .navigationTransition(
+          .zoom(
+            sourceID: sourceID,
+            in: namespace
+          )
+        )
+        .interactiveDismissDisabled()
+    } else {
+      content
+    }
+#else
+    content
+#endif
+  }
+  
+  @ViewBuilder public func matchedTransitionSource(
+    id: some Hashable,
+    in namespace: Namespace.ID
+  ) -> some View {
+#if os(iOS)
+    if #available(iOS 18, *) {
+      content.matchedTransitionSource(
+        id: id,
+        in: namespace
+      )
+    } else {
+      content
+    }
+#elseif os(macOS)
+    if #available(macOS 15.0, *) {
+      content.matchedTransitionSource(
+        id: id,
+        in: namespace
+      )
+    } else {
+      content
+    }
+#else
+    content
+#endif
+  }
+  
+  @ViewBuilder public func imagePlaygroundSheet(
+    isPresented: Binding<Bool>,
+    sourceImage: Image? = nil,
+    onCompletion: @escaping (URL) -> Void,
+    onCancellation: (() -> Void)? = nil
+  ) -> some View {
+#if os(iOS)
+    if #available(iOS 18.1, *) {
+      if ImagePlaygroundViewController.isAvailable {
+        content
+          .imagePlaygroundSheet(
+            isPresented: isPresented,
+            sourceImage: sourceImage,
+            onCompletion: onCompletion,
+            onCancellation: onCancellation
+          )
+      } else {
+        content
+      }
+    } else {
+      content
+    }
+#elseif os(macOS)
+    if #available(macOS 15.1, *) {
+      if ImagePlaygroundViewController.isAvailable {
+        content
+          .imagePlaygroundSheet(
+            isPresented: isPresented,
+            sourceImage: sourceImage,
+            onCompletion: onCompletion,
+            onCancellation: onCancellation
+          )
+      } else {
+        content
+      }
+    } else {
+      content
+    }
+#else
+    content
+#endif
   }
 }
